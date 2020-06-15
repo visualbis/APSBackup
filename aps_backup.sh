c1a5c23e417924821c0c0f92e4599cf7ca4f5ef1
Devaraj <devaraj@wp.pl>
2020-06-15 11:18:01 +0530

    Mon Jun 15 11:18:01 IST 2020

--- aps_backup.sh
+++ aps_backup.sh
@@ -1,5 +1,4 @@
-
-mongodump --uri=mongodb://$1:$2@ds249092.mlab.com:49092/vbx_current;
+./mongodump --uri=mongodb://$1:$2@ds249092.mlab.com:49092/vbx_current;
 rm -r ./vbx_current;
 cp -r dump/* .;
 rm -r dump;
